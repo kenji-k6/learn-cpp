8f3b32cf7645a0ad6e268ca2f0b197a514eee3b5
Kenji Yuri Nakano <kenji.y.nakano@gmail.com>
2025-02-05 13:44:03 +0100

    Chapter 1 completed

--- chapter1.cpp
+++ chapter1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 void chap1_5();
+void chap1_6();
+void chap1_11();
 
 int main() {
   int x{ 1 };
@@ -10,7 +12,8 @@ int main() {
   std::cout << x << y << std::endl;
 
   chap1_5();
-
+  chap1_6();
+  chap1_11();
   return 0;
 }
 
@@ -20,4 +23,22 @@ void chap1_5() {
   std::cin >> x;
 
   std::cout << "You entered: " << x << "\n";
+}
+
+void chap1_6() {
+  int x{};
+
+  std::cout << x << "\n" << "------------" << "\n";
+
+}
+
+
+
+void chap1_11() {
+  int x{ };
+
+  std::cout << "Entern an integer: ";
+  std::cin >> x;
+  std::cout << "Double that number is: " << 2*x << "\n";
+  
 }
